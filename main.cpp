de549257005d0bd85b22d35b322b74e29682b3b9
Nyew62 <nyew@umich.edu>
2025-04-24 20:51:04 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -33,9 +33,68 @@ ostream& operator<<(ostream& out, Profile& user){
     out << user.fulln << "---" << user.state;
     return out;
 }
- bool searchDeque(deque<Profile>& pq, Profile&)
+ bool searchDeque(deque<Profile>& pq, Profile&);
 
 int main() {
+MyList<Profile> pMyList;
+queue<Profile> pqueue;
+deque<Profile> pdeque;
+Profile temp;
+string line;
+
+ifstream file("presidentsWstates");
+if (file.is_open()){
+    while(getline(file,line)){
+        stringstream ss(line);
+        getline(ss, temp.fulln,'\t');
+        getline(ss, temp.state,'\t');
+        pMyList.insertHead(temp);
+        pqueue.push(temp);
+        pdeque.push_front(temp);
+    }
+
+
+}else{
+    cout<<"cant open file"<<endl;
+}
+
+cout << "List: "<<endl;
+pMyList.display();
+
+
+cout << "Queue: "<< endl;
+while(!pqueue.empty()){
+    cout << pqueue.front()<<endl;
+    pqueue.pop();
+}
+
+
+for (deque<Profile>::iterator dIte=pdeque.begin(); dIte!=pdeque.end();dIte++){
+    cout<<*dIte<<endl;
+}
+
+for (size_t i=0; i<pdeque.size(); i++){
+    cout << pdeque[i]<<endl;
+
+    temp.fulln="John Tyler";
+    if(searchDeque(pdeque,temp)){
+        cout <<"found: "<<temp<<endl;
+    }
+    else{
+        cout <<"name not found: "<<temp<<endl;
+    }
+}
+return 0;
+
 
 }
 
+bool searchDeque(deque<Profile> &pq, Profile &president){
+    for(Profile &p:pq)
+        if(p==president){
+            president=p;
+            return true;
+        }
+        return false;
+
+}
